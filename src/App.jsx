5c49e0194c666e6e2ffb48acdec876c2fc9f0674
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-12-25 14:06:26 +0400

    reactQr

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='container'>
+        <div className='blue'>
+          <img className='img' src="/ass/qr-code.svg"/>
+            </div>
+          <h1>Improve your front-end skills by building projects</h1>
+        <h2>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
